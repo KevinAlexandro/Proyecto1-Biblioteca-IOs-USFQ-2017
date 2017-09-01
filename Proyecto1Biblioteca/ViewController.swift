c1ea56d936c1065ec03b60c71eb66d303aaa3a07
user130165 <user130165@ny211.local>
2017-09-01 07:23:57 -0700

    Initial Commit

---
+++ Proyecto1Biblioteca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto1Biblioteca
+//
+//  Created by user130165 on 9/1/17.
+//  Copyright © 2017 KevinGonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
